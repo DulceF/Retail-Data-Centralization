3178a7ccf56667550185b0285d556484302d36fc
DulceF <86166245+DulceF@users.noreply.github.com>
2024-02-20 18:25:46 +0000

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import psycopg2
 import requests
 import json
+from pandas import json_normalize
 
 
 pdf_path = "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
@@ -16,52 +17,51 @@ file_path = r"D:\Aicore_projects\Data Manipulation\Multinational Retail Data\db_
         #(Milestone2, Task2, Step1)
         #This class is used to extract data from different data sources
 class DataExtractor:
-                def __init__ (self): 
-                        pass
+        def __init__ (self): 
+                pass
 
         #(Milestone2, Task3, Step2)
         ##This will read the credentials of the yaml file and return a dictionary of the credentials
-                def read_db_creds(self,file_path):
-                        with open(file_path, "r") as file:
-                                data = yaml.safe_load(file)
-                        return data
+        def read_db_creds(self,file_path):
+                with open(file_path, "r") as file:
+                        data = yaml.safe_load(file)
+                return data
 
         #(Milestone2, Task3, Step3)
         #Create a method to read credentials and initialise and return an sqlalchemy database engine
-                def init_db_engine(self,file_path):
-                        credentials = self.read_db_creds(file_path)
-                        print(credentials)       
-                        #Extract the credentials
-                        host = credentials["host"]
-                        password = credentials["password"]
-                        user = credentials["user"]
-                        database = credentials["database"]
-                        port = credentials["port"]
-                        
-                        #Create and return the SQLAlchemy database engine
-                        engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")      
-                        return engine
+        def init_db_engine(self,file_path):
+                credentials = self.read_db_creds(file_path)
+                print(credentials)       
+                #Extract the credentials
+                host = credentials["host"]
+                password = credentials["password"]
+                user = credentials["user"]
+                database = credentials["database"]
+                port = credentials["port"]
+                #Create and return the SQLAlchemy database engine
+                engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")      
+                return engine
 
         #(Milestone2, Task3, Step4)  
         # Using the engine from init_db_engine, create a method to list all the tables in the database
-                def list_db_tables(self, file_path):
-                        engine = self.init_db_engine(file_path)
-                        inspector = inspect(engine)
-                        tables = inspector.get_table_names()
-                        return tables
+        def list_db_tables(self, file_path):
+                engine = self.init_db_engine(file_path)
+                inspector = inspect(engine)
+                tables = inspector.get_table_names()
+                return tables
 
         #(Milestone2, Task3, Step5) 
         #In this step we will extract the database table to a pandas DataFrame  
 
-                def read_rds_table(self, file_path, table_name):
-                        #Connect to PostgreSQL server
-                        engine = self.init_db_engine(file_path) #Get the engine from this function
-                        dbConnection = engine.connect()
-                        #Read data from PostgreSQL database table and load into a DataFrame instance
-                        df = pd.read_sql_table(table_name, dbConnection)   
-                        return df
+        def read_rds_table(self, file_path, table_name):
+                #Connect to PostgreSQL server
+                engine = self.init_db_engine(file_path) #Get the engine from this function
+                dbConnection = engine.connect()
+                #Read data from PostgreSQL database table and load into a DataFrame instance
+                df = pd.read_sql_table(table_name, dbConnection)   
+                return df
 
-                def retrieve_pdf_data(self,pdf_path):
+        def retrieve_pdf_data(self,pdf_path):
                         #Read the PDF file and store the list of DataFrames in "tables". 
                         # Tabula returns a list of DataFrames,one for each page of the PDF that contains tabular data
                         pdf_tables = tabula.read_pdf(pdf_path, pages="all")
@@ -72,13 +72,12 @@ class DataExtractor:
                 #               *** API's *** Milestine 2 Task 5
 
         #Step1
-                def list_number_of_stores(self,url,headers):
+        def list_number_of_stores(self,list_url,headers):
                         #Send a GET request to Return the number of stores to extract
-                        response = requests.get(url, headers= headers) 
+                        response = requests.get(list_url, headers= headers) 
                         #Check if the API request was successful
                         if response.status_code == 200:
-                        
-                                #Parse the JSON response
+                        #Parse the JSON response
                                 data = response.json()
                                 return data
                         else:
@@ -86,29 +85,26 @@ class DataExtractor:
                                 print(f"Response content: {response.text}")
                                 return None
         #Step3
-                def retrieve_stores_data(self,url_all_stores,headers):
-                        response = requests.get(url_all_stores,headers=headers)
-        
-                        #Check if the API request was succeccful
+        def retrieve_stores_data(self,base_url,headers):
+                        
+                        store_info = []
+                        response = requests.get(base_url,headers=headers)
+                        
+                                #Check if the API request was successful
                         if response.status_code == 200:
-                                print("request successful")
-                                 #Parse the JSON response
+                                #Parse the JSON response
                                 data = response.json()
-                                store_info = []
-                                
-                                for x in data:
-                                        store_info.append(x)
-                                
-                                 
-                                print(data)
+                                #for store_data in data: 
+                                        #Save the information
+                                        #store_info.append(store_data)
+                                store_info = json_normalize(data)
                         else:
                                 print(f"request unsuccessful. Status code: {response.status_code}")
                                 print(f"Response content: {response.text}")
-                                data = None
-                                            
-                     
-                        print(data)
-                        return data
+                                
+                        #Create a Pandas Dataframe                  
+                        df = pd.DataFrame(store_info)
+                        return df
                         
 
 if  __name__ == "__main__":
@@ -118,23 +114,40 @@ if  __name__ == "__main__":
                         #credentials = read_db_creds(file_path)
                         #engine = init_db_engine()
                         combined_df = extractor.retrieve_pdf_data(pdf_path)
+                               
                         #Define the URL and headers for the API request
-                        url =  "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
+                        list_url =  "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores"
                         headers = {"x-api-key" : "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
-                        data = extractor.list_number_of_stores(url, headers)
-                        #Number of stores: {"statusCode": 200, "number_stores": 451}
-                        url_all_stores = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}"
-                        stores_data = extractor.retrieve_stores_data(url_all_stores,headers)
+                        data = extractor.list_number_of_stores(list_url,headers)
                         
-                        print(stores_data)
-
-
-
-
-        
-
-
-       
-
+                        
+                        base_url = "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/"
+                        
+                        #List containing store numbers
+                        store_numbers = range(1,452)
+                        #Loop over the store number and add them to the URL
+                        data_frames = []
+                        for store_number in store_numbers:
+                                #print(f"This data is from store number: {store_number}")
+                                #This is the url for the API request
+                                url = f"{base_url}{store_number}" #Concatenate the url and the store number (join)
+                                
+                                #Make the API request 
+                                stores_data = extractor.retrieve_stores_data(url,headers)
+                                #print(f"Response data for store {store_number}:", stores_data)
+                                
+                                #Append the response to the data_frames list
+                                data_frames.append(stores_data)
+                                
+                        #Concatenate all the dataframes into a single dataframe
+                        if data_frames:
+                                result_df = pd.concat(data_frames)       
+                                #Save to Excel file
+                                print(result_df)
+                                result_df.to_excel("output_of_stores.xlsx", index = False)
+                        else:
+                                print("No data found")               
+                                
+                                
 
 
